8ca3ff8418c3e3087b9b06fc005383c635852bf5
JustACoder <58445942+CodingTheUniverse@users.noreply.github.com>
2021-03-25 11:53:33 -0400

    Create block.py

---
+++ block.py
@@ -0,0 +1,14 @@
+'''
+    ---------------------------------------------
+    -  Simple Python Script Blocks All Ports !  -
+    -           Very Simple Script !            -
+    ---------------------------------------------
+'''
+import os
+import platform
+
+if platform == "linux" or platform == "linux2":
+    for i in range(65535):
+        os.system("-A INPUT -p tcp --destination-port {} -j DROP".format(i))
+        # Also Block Udp Traffic !
+        os.system("iptables -I INPUT -p udp --dport {} -j DROP".format(i))
