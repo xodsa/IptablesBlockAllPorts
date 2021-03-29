64b5ab953e018e6eb1d005acc1dacfc14e11ddaf
JustACoder <58445942+CodingTheUniverse@users.noreply.github.com>
2021-03-28 20:53:26 -0400

    Update block.py

--- block.py
+++ block.py
@@ -9,6 +9,6 @@ import platform
 
 if platform == "linux" or platform == "linux2":
     for i in range(65535):
-        os.system("-A INPUT -p tcp --destination-port {} -j DROP".format(i))
+        os.system("iptables -A INPUT -p tcp --destination-port {} -j DROP".format(i))
         # Also Block Udp Traffic !
         os.system("iptables -I INPUT -p udp --dport {} -j DROP".format(i))
